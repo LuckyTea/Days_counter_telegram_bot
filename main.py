153f5192e2b32d009b63af83eac06a6cbb7f2bc3
luckytea <gearpanda@gmail.com>
2017-07-17 23:26:22 +0300

    - warn() - now everything in echo()

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ class Init():
 
 
 def main():
-    warn(f'Good day, sir!')
+    echo(msg='Good day, sir!', warn=True)
     if not get_info():
-        warn('Bot is down')
+        echo(msg='Bot is down', warn=True)
         return
     if not init_db():
-        warn('Can\'t initialize DB')
+        echo(msg='Can\'t initialize DB', warn=True)
         return
-    warn(f'Now in DB {I.LAST_PRECIOUS} records')
-    warn('Bot work well. Getting messages...')
+    echo(msg=f'Now in DB {I.LAST_PRECIOUS} records\nBot work well. Getting messages...', warn=True)
     run = 1
     while run is 1:
         msg = get_updates(offset=I.LAST_ID)
@@ -109,7 +108,7 @@ def handle_msg(msg, last_update):
             msg_text = msg_header['sticker']['emoji']
         except:
             msg_text = '...'
-            warn(f'!!!Some stange message: {msg_header}')
+            echo(msg=f'!!!Some stange message: {msg_header}', warn=True)
     # shutdown button
     if msg_text.lower() == '/bot_stop' and msg_chat_id == config.owner_id and I.LAST_ID == msg_id:
         send_sticker(chat_id=msg_chat_id, file_id='CAADAgADwwQAAvoLtgiyQa_zvBHWHwI')
@@ -118,35 +117,39 @@ def handle_msg(msg, last_update):
     # start counting
     elif msg_text.lower()[:20] == '!bot start counting ' and I.LAST_ID == msg_id:
         if len(msg_text[20:]) > 1000:
-            send_msg(chat_id=msg_chat_id, text='Message is too long!')
+            send_msg(chat_id=msg_chat_id, msg='Message is too long!')
         else:
             counting_start(chat_id=msg_chat_id, msg_date=msg_date, msg=msg_text)
     # show counting
     elif msg_text.lower()[:10] == '!bot show ':
-        counting_show(chat_id=msg_chat_id, msg_date=msg_date, req=msg_text[10:])
+        counting_show(chat_id=msg_chat_id, msg_date=msg_date, msg=msg_text[10:])
     # reset counting
     elif msg_text.lower()[:11] == '!bot reset ':
-        counting_reset(chat_id=msg_chat_id, msg_date=msg_date, req=msg_text[11:])
+        counting_reset(chat_id=msg_chat_id, msg_date=msg_date, msg=msg_text[11:])
     # delete counting
     elif msg_text.lower()[:12] == '!bot delete ':
-        counting_delete(chat_id=msg_chat_id, msg_date=msg_date, req=msg_text[12:])
+        counting_delete(chat_id=msg_chat_id, msg_date=msg_date, msg=msg_text[12:])
     # show help
     elif msg_text.lower()[:9] == '!bot help' and len(msg_text) == 9:
-        send_help(chat_id=msg_chat_id)
+        send_help(chat_id=msg_chat_id, msg_date=msg_date)
     # echo
     elif I.LAST_ID == msg_id:
-        echo(id=msg_id, date=msg_date, user=msg_user, chat=msg_chat_id, text=msg_text)
+        echo(id=msg_id, date=msg_date, user=msg_user, chat=msg_chat_id, msg=msg_text)
     I.LAST_ID = msg_id + 1
     return True
 
 
-def echo(id='...', date='...', user='...', chat='...', text='...'):
-    print(f'At {date} user {user} from {chat} post message #{id}:\n{text}\n{"="*70}')
+def echo(id='...', date=time.time(), user='...', chat='...', msg='...', warn=False):
+    date = datetime.fromtimestamp(int(date)).strftime('%d.%m.%Y - %H:%M:%S')
+    temp = (f'At {date} user {user} from {chat} post message #{id}:\n{msg}\n{"="*70}')
+    if warn:
+        temp = (f'\x1b[0;31;40m{temp}\x1b[0m')
+    print(temp)
 
 
-def send_msg(chat_id=config.owner_id, text='Good day, sir!'):
-    text = urllib.parse.quote_plus(text)
-    req = f'{I.HOST}/sendMessage?chat_id={chat_id}&text={text}&parse_mode=markdown'
+def send_msg(chat_id=config.owner_id, msg='Good day, sir!'):
+    msg = urllib.parse.quote_plus(msg)
+    req = f'{I.HOST}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=markdown'
     return action(req)
 
 
@@ -188,22 +191,22 @@ def counting_start(chat_id, msg_date, msg):
                 break
             except sqlite3.IntegrityError:
                 I.LAST_PRECIOUS += 1
-        send_msg(chat_id=chat_id, text=f'I {msg[5:]}')
-        warn(f'\tFor {chat_id} add {name} since {date}')
+        send_msg(chat_id=chat_id, msg=f'I {msg[5:]}')
+        echo(date=msg_date, chat=chat_id, msg=msg, warn=True)
         I.LAST_PRECIOUS += 1
     else:
-        send_msg(chat_id=chat_id, text=f'Wrong command')
-        warn(f'\tFailed to add: {msg} for {chat_id}')
+        send_msg(chat_id=chat_id, msg=f'Wrong command')
+        echo(date=msg_date, chat=chat_id, msg=f'Failed to add: {msg}', warn=True)
     connect.commit()
     connect.close()
 
 
-def counting_show(chat_id, msg_date, req):
+def counting_show(chat_id, msg_date, msg):
     connect = sqlite3.connect('precious.db')
     c = connect.cursor()
     temp = ''
     try:
-        if req == 'all':
+        if msg == 'all':
             c.execute("SELECT * FROM MAIN WHERE CHAT_ID=?", (str(chat_id), ))
             result = c.fetchall()
             for row in result:
@@ -215,63 +218,63 @@ def counting_show(chat_id, msg_date, req):
                 if len(temp + row[2]) < 4000:
                     temp += f'Day\'s since {row[2]}: {date}️⃣\n'
                 else:
-                    send_msg(chat_id=chat_id, text=temp)
+                    send_msg(chat_id=chat_id, msg=temp)
                     temp = ''
                     temp += f'Day\'s since {row[2]}: {date}️⃣\n'
         else:
-            c.execute("SELECT * FROM MAIN WHERE CHAT_ID=? AND NAME=?", (str(chat_id), str(req)))
+            c.execute("SELECT * FROM MAIN WHERE CHAT_ID=? AND NAME=?", (str(chat_id), str(msg)))
             result = c.fetchone()
             date = int(msg_date - int(result[3])) // 86400
             date = '️⃣'.join(tuple(str(date)))
             temp += f'Day\'s since {result[2]}: {date}️⃣\n'
     except:
-        warn(f'{sys.exc_info()}')
+        echo(date=msg_date, chat=chat_id, msg=sys.exc_info(), warn=True)
         temp = 'Nothing to show'
     connect.close()
-    send_msg(chat_id=chat_id, text=temp)
-    warn(f'\tFor {chat_id} show {req} at {msg_date}')
+    send_msg(chat_id=chat_id, msg=temp)
+    echo(date=msg_date, chat=chat_id, msg=f'!bot show {msg}', warn=True)
 
 
-def counting_reset(chat_id, msg_date, req):
+def counting_reset(chat_id, msg_date, msg):
     connect = sqlite3.connect('precious.db')
     c = connect.cursor()
     temp = ''
     try:
-        c.execute("UPDATE MAIN SET TIME=? WHERE CHAT_ID=? AND NAME=?", (int(time.time()), str(chat_id), str(req)))
+        c.execute("UPDATE MAIN SET TIME=? WHERE CHAT_ID=? AND NAME=?", (int(time.time()), str(chat_id), str(msg)))
         result = c.fetchall()
         if connect.total_changes is 0:
             raise Exception('Nothing to reset')
         temp = f'Resetting {connect.total_changes} records'
         connect.commit()
     except:
-        warn(f'{sys.exc_info()}')
+        echo(date=msg_date, chat=chat_id, msg=sys.exc_info(), warn=True)
         temp = 'Nothing to reset'
     connect.close()
-    send_msg(chat_id=chat_id, text=temp)
-    warn(f'\tFor {chat_id} show {req} at {msg_date}')
+    send_msg(chat_id=chat_id, msg=temp)
+    echo(date=msg_date, chat=chat_id, msg=f'!bot reset {msg}', warn=True)
 
 
-def counting_delete(chat_id, msg_date, req):
+def counting_delete(chat_id, msg_date, msg):
     connect = sqlite3.connect('precious.db')
     c = connect.cursor()
     temp = ''
     try:
-        result = c.execute("SELECT TIME FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(req)))
+        result = c.execute("SELECT TIME FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(msg)))
         date = c.fetchone()
-        c.execute("DELETE FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(req)))
+        c.execute("DELETE FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(msg)))
         date = round(int(time.time() - int(date[0])) / 86400)
         date = '️⃣'.join(tuple(str(date)))
-        temp = f'I delete counting for {req} since {date}️⃣ days!'
+        temp = f'I delete counting for {msg} since {date}️⃣ days!'
     except:
-        warn(f'{sys.exc_info()}')
+        echo(date=msg_date, chat=chat_id, msg=sys.exc_info(), warn=True)
         temp = 'Nothing to delete'
     connect.commit()
     connect.close()
-    send_msg(chat_id=chat_id, text=temp)
-    warn(f'\tFor {chat_id} show {req} at {msg_date}')
+    send_msg(chat_id=chat_id, msg=temp)
+    echo(date=msg_date, chat=chat_id, msg=f'!bot delete {msg}, warn=True)
 
 
-def send_help(chat_id):
+def send_help(chat_id, msg_date):
     text = '''
 *"Days since"* commands:
 `!bot start counting for <smth>` - start unique counting for user|group with name `<smth>`
@@ -282,12 +285,9 @@ def send_help(chat_id):
 `!bot delete <smth>` - delete counter for event `<smth>`
 `!bot help` - idk
 '''
-    send_msg(chat_id=chat_id, text=text)
-    return
+    send_msg(chat_id=chat_id, msg=text)
+    echo(date=msg_date, chat=chat_id, msg='!bot help', warn=True)
 
-
-def warn(msg):
-    print(f'\x1b[0;31;40m{msg}\x1b[0m\n{"="*70}')
 
 if __name__ == '__main__':
     I = Init()
