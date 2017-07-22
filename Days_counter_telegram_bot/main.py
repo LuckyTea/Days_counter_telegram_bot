80b383e47c4152e57f84a28bc6ea7add0416f070
luckytea <gearpanda@gmail.com>
2017-07-22 15:01:43 +0300

    * fix missing files

---
+++ Days_counter_telegram_bot/main.py
@@ -0,0 +1,297 @@
+'''
+Days counter - telegram bot
+Can count days since smth, for example - last jewish tricks
+'''
+
+from datetime import datetime
+import json
+import re
+import requests
+import sqlite3
+import sys
+import time
+import urllib
+
+
+class Init():
+    def __init__(self):
+        import config
+        self.HOST = f'https://api.telegram.org/bot{config.token}'
+        self.LAST_ID = None
+        self.LAST_PRECIOUS = 0
+        self.OWNER_ID = config.owner_id
+        self.DB_NAME = config.db_name
+
+
+def main():
+    echo(msg='Good day, sir!', warn=True)
+    if not get_status(req=f'{I.HOST}/getMe'):
+        return echo(msg='Bot is down', warn=True)
+    if not init_db():
+        return echo(msg='Can\'t initialize DB', warn=True)
+    echo(msg=f'Now in DB {I.LAST_PRECIOUS} records\nBot work well. Getting messages...', warn=True)
+    run = 1
+    while run is 1:
+        msg = get_updates(offset=I.LAST_ID)
+        if len(msg["result"]) > 0:
+            for last_update in range(len(msg['result'])):
+                if handle_msg(msg, last_update) is 0:
+                    run = 0
+        time.sleep(0.5)
+
+
+def init_db():
+    try:
+        connect = sqlite3.connect(I.DB_NAME)
+        c = connect.cursor()
+        try:
+            c.execute('''CREATE TABLE MAIN
+                     (ID      INT PRIMARY KEY   NOT NULL,
+                     CHAT_ID              INT   NOT NULL,
+                     NAME                TEXT   NOT NULL,
+                     TIME                 INT   NOT NULL);''')
+        except sqlite3.OperationalError:
+            c.execute('SELECT COUNT(*) FROM MAIN')
+            I.LAST_PRECIOUS = int(c.fetchone()[0])
+        connect.close()
+        return 1
+    except:
+        echo(msg=f'init_db() exit code 0 {sys.exc_info()}', warn=True)
+        return 0
+
+
+def action(req):
+    response = requests.get(req)
+    content = response.content.decode('utf8')
+    return content
+
+
+def get_json(req):
+    content = action(req)
+    content = json.loads(content)
+    return content
+
+
+def get_status(req):
+    response = get_json(req)
+    return response["ok"]
+
+
+def get_updates(offset=None, timeout=100):
+    req = f'{I.HOST}/getUpdates?timeout={timeout}'
+    if offset:
+        req += f'&offset={offset}'
+    msg = get_json(req)
+    return msg
+
+
+def handle_msg(msg, last_update):
+    # handling message id
+    msg_id = msg['result'][last_update]['update_id']
+    # handling message type
+    try:
+        msg_header = msg['result'][last_update]['message']
+    except:
+        msg_header = msg['result'][last_update]['edited_message']
+    # handling message date time
+    msg_date = msg_header['date']
+    # handling message author
+    msg_user = msg_header['from']['username']
+    # handling message chat id
+    msg_chat_id = msg_header['chat']['id']
+    # handling message text
+    try:
+        msg_text = msg_header['text']
+    except:
+        try:
+            msg_text = msg_header['sticker']['emoji']
+        except:
+            msg_text = '...'
+            echo(msg=f'!!! Some stange message: {msg_header}', warn=True)
+    # logging
+    echo(msg_id, msg_date, msg_user, msg_chat_id, msg_text)
+    I.LAST_ID = msg_id + 1
+    # shutdown button
+    if msg_text.lower() == '/bot_stop' and msg_chat_id == I.OWNER_ID:
+        get_updates(offset=msg_id+1, timeout=0)
+        send_msg(msg_chat_id, '🏁')
+        return 0
+    # start counting
+    elif msg_text.lower()[:20] == '!bot start counting ':
+        if len(msg_text[20:]) > 200:
+            return send_msg(msg_chat_id, 'This message is too long. It cannot be record.')
+        else:
+            return counting_start(msg_chat_id, msg_date, msg_text)
+    # show counting
+    elif msg_text.lower()[:10] == '!bot show ':
+        return counting_show(msg_chat_id, msg_date, msg_text[10:])
+    # reset counting
+    elif msg_text.lower()[:11] == '!bot reset ':
+        return counting_reset(msg_chat_id, msg_date, msg_text[11:])
+    # delete counting
+    elif msg_text.lower()[:12] == '!bot delete ':
+        return counting_delete(msg_chat_id, msg_date, msg_text[12:])
+    # show help
+    elif msg_text.lower()[:9] == '!bot help' and len(msg_text) == 9:
+        return send_help(msg_chat_id)
+    return 1
+
+
+def echo(id='...', date=time.time(), user='...', chat='...', msg='...', warn=False):
+    date = datetime.fromtimestamp(int(date)).strftime('%d.%m.%Y - %H:%M:%S')
+    temp = (f'At {date} user {user} from {chat} post message #{id}:\n{msg}\n{"="*70}')
+    if warn:
+        temp = (f'\x1b[0;31;40m{temp}\x1b[0m')
+    print(temp)
+    return temp
+
+
+def send_msg(chat_id, msg='Good day, sir!'):
+    msg = urllib.parse.quote_plus(msg)
+    req = f'{I.HOST}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=markdown'
+    return action(req)
+
+
+def counting_start(chat_id, msg_date, msg):
+    connect = sqlite3.connect(I.DB_NAME)
+    c = connect.cursor()
+    name = ''
+    # get timestamp of date
+    timestamp = int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple()))
+    # lazy counter
+    if re.search(r'^!bot start counting for (.*)', msg):
+        name = msg[24:]
+    # words counter
+    elif re.search(r'^!bot start counting since (now|today|tomorrow|yesterday) for (.*)', msg):
+        res = re.match(r'^!bot start counting since (now|today|tomorrow|yesterday) for (.*)', msg)
+        name = res.group(2)
+        if res.group(1) == 'tomorrow':
+            timestamp += 86400
+        elif res.group(1) == 'yesterday':
+            timestamp -= 86400
+    # date counter
+    elif re.search(r'!bot start counting since ([\d]{2}[.][\d]{2}[.][\d]{4}) for (.*)', msg):
+        res = re.match(r'!bot start counting since ([\d]{2}[.][\d]{2}[.][\d]{4}) for (.*)', msg)
+        name = res.group(2)
+        try:
+            timestamp = int(time.mktime(datetime.strptime(res.group(1), "%d.%m.%Y").timetuple()))
+            if timestamp < 0:
+                raise Exception
+        # fix for windows
+        except Exception as e:
+            timestamp = 0
+            msg += '. But honestly i can\'t count earlier than 01.01.1970.'
+    if name != '':
+        while 1:
+            try:
+                c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (I.LAST_PRECIOUS, chat_id, name, timestamp))
+                send_msg(chat_id=chat_id, msg=f'I {msg[5:]}')
+                I.LAST_PRECIOUS += 1
+                break
+            except sqlite3.IntegrityError:
+                I.LAST_PRECIOUS += 1
+    else:
+        send_msg(chat_id=chat_id, msg=f'Wrong command')
+    connect.commit()
+    connect.close()
+
+
+def counting_show(chat_id, msg_date, msg):
+    connect = sqlite3.connect(I.DB_NAME)
+    c = connect.cursor()
+    temp = ''
+    try:
+        if msg == 'all':
+            c.execute("SELECT * FROM MAIN WHERE CHAT_ID=?", (str(chat_id), ))
+            result = c.fetchall()
+            if len(result) is 0:
+                raise Exception
+            for row in result:
+                if msg_date < int(row[3]):
+                    date = 0  # Don't count for the future
+                else:
+                    date = int(msg_date - int(row[3])) // 86400
+                date = '️⃣'.join(tuple(str(date)))
+                if len(temp + row[2]) < 1000:
+                    temp += f'Day\'s since {row[2]}: {date}️⃣\n'
+                else:
+                    send_msg(chat_id=chat_id, msg=temp)
+                    temp = ''
+                    temp += f'Day\'s since {row[2]}: {date}️⃣\n'
+        else:
+            c.execute("SELECT * FROM MAIN WHERE CHAT_ID=? AND NAME=?", (str(chat_id), str(msg)))
+            result = c.fetchone()
+            if msg_date < int(result[3]):
+                    date = 0  # Don't count for the future
+            else:
+                date = int(msg_date - int(result[3])) // 86400
+            date = '️⃣'.join(tuple(str(date)))
+            temp += f'Day\'s since {result[2]}: {date}️⃣\n'
+    except:
+        connect.close()
+        temp = 'Nothing to show'
+        send_msg(chat_id=chat_id, msg=temp)
+        echo(date=msg_date, chat=chat_id, msg=f'{msg} - {sys.exc_info()}', warn=True)
+        return 0
+    connect.close()
+    send_msg(chat_id=chat_id, msg=temp)
+    return 1
+
+
+def counting_reset(chat_id, msg_date, msg):
+    try:
+        connect = sqlite3.connect(I.DB_NAME)
+        c = connect.cursor()
+        temp = ''
+        # reset all by design
+        c.execute("UPDATE MAIN SET TIME=? WHERE CHAT_ID=? AND NAME=?", (int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())), str(chat_id), str(msg)))
+        result = c.fetchall()
+        if connect.total_changes is 0:
+            raise Exception
+        temp = f'Resetting {connect.total_changes} records'
+        connect.commit()
+        connect.close()
+    except:
+        echo(date=msg_date, chat=chat_id, msg=sys.exc_info(), warn=True)
+        temp = 'Nothing to reset'
+    send_msg(chat_id=chat_id, msg=temp)
+
+
+def counting_delete(chat_id, msg_date, msg):
+    connect = sqlite3.connect(I.DB_NAME)
+    c = connect.cursor()
+    temp = ''
+    try:
+        result = c.execute("SELECT TIME FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(msg)))
+        date = c.fetchone()
+        c.execute("DELETE FROM MAIN WHERE CHAT_ID=? AND ID=(SELECT MIN(ID) FROM MAIN WHERE CHAT_ID=? AND NAME=?)", (str(chat_id), str(chat_id), str(msg)))
+        date = round(int(time.time() - int(date[0])) // 86400)
+        date = '️⃣'.join(tuple(str(date)))
+        temp = f'I delete counting for {msg} since {date}️⃣ days!'
+    except:
+        echo(date=msg_date, chat=chat_id, msg=sys.exc_info(), warn=True)
+        temp = 'Nothing to delete'
+    connect.commit()
+    connect.close()
+    send_msg(chat_id=chat_id, msg=temp)
+
+
+def send_help(chat_id):
+    text = '''
+*"Days since"* commands:
+`!bot start counting for <smth>` - start unique counting for user|group with name `<smth>`
+`!bot start counting since <dd.mm.yyy> for <smth>` - start unique counting for user|group with name `<smth>` since `<dd.mm.yyy>`
+`!bot show <smth>` - show how many days sice event `<smth>`
+`!bot show all` - show all events for user|group
+`!bot reset <smth>` - reset counter for event `<smth>`
+`!bot delete <smth>` - delete counter for event `<smth>`
+`!bot help` - idk
+'''
+    send_msg(chat_id=chat_id, msg=text)
+
+
+if __name__ == '__main__':
+    I = Init()
+    main()
+else:
+    I = Init()
---
+++ Days_counter_telegram_bot/test.py
@@ -0,0 +1,415 @@
+from datetime import datetime
+import json
+import os
+import sqlite3
+import time
+import unittest
+from unittest.mock import patch
+import main as m
+
+
+class init_db(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+        m.I.DB_NAME = 'test_case.db'
+
+    def test_init_db_succ(self):
+        self.assertEqual(m.init_db(), 1)
+
+    def test_init_db_table_already_exist(self):
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute('''CREATE TABLE MAIN
+                 (ID      INT PRIMARY KEY   NOT NULL,
+                 CHAT_ID              INT   NOT NULL,
+                 NAME                TEXT   NOT NULL,
+                 TIME                 INT   NOT NULL);''')
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (1, 2, 'Test Value 1', 4)")
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (5, 6, 'Test Value 2', 8)")
+        connect.commit()
+        connect.close()
+        self.assertEqual(m.init_db(), 1)
+        self.assertEqual(m.I.LAST_PRECIOUS, 2)
+
+    def test_init_db_cant_create(self):
+        m.I.DB_NAME = '//'
+        self.assertEqual(m.init_db(), 0)
+        self.assertEqual(m.I.LAST_PRECIOUS, 0)
+
+    def tearDown(self):
+        try:
+            os.remove('test_case.db')
+        except FileNotFoundError:
+            ...
+
+
+class action(unittest.TestCase):
+    def test_action(self):
+        req = 'https://api.telegram.org/bot'
+        self.assertEqual(m.action(req), '{"ok":false,"error_code":404,"description":"Not Found"}')
+
+
+class get_json(unittest.TestCase):
+    @patch('__main__.m.action', return_value='{"ok":"true"}')
+    def test_get_json(self, action):
+        req = None
+        self.assertEqual(m.get_json(req), json.loads('{"ok":"true"}'))
+
+
+class get_status(unittest.TestCase):
+    def setUp(self):
+        m.req = None
+
+    @patch('__main__.m.get_json', return_value=json.loads('{"ok":"true"}'))
+    def test_get_status_up(self, get_json):
+        self.assertEqual(m.get_status(m.req), "true")
+
+    @patch('__main__.m.get_json', return_value=json.loads('{"ok":"false"}'))
+    def test_get_status_down(self, get_json):
+        self.assertEqual(m.get_status(m.req), "false")
+
+
+class handle_msg(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+
+    def generate_msg(edit=False, wtf=False, sticker=False, case='Show must go on!'):
+        msg = json.loads('''
+            {"ok": "True",
+            "result": [{
+                "update_id": 0,
+                "message": {
+                    "message_id": 41,
+                    "from": {
+                        "id": 24101991,
+                        "first_name": "Freddie",
+                        "last_name": "Mercury",
+                        "username": "MrBadGuy",
+                        "language_code": "en-US"\
+                    },
+                    "chat": {
+                        "id": 24101991,
+                        "first_name": "Freddie",
+                        "last_name": "Mercury",
+                        "username": "MrBadGuy",
+                        "type": "private"
+                    },
+                    "date": 688251600,
+                    "text": ""
+                },
+                "edited_message": {
+                    "message_id": 41,
+                    "from": {
+                        "id": 24101991,
+                        "first_name": "Freddie",
+                        "last_name": "Mercury",
+                        "username": "MrBadGuy",
+                        "language_code": "en-US"\
+                    },
+                    "chat": {
+                        "id": 24101991,
+                        "first_name": "Freddie",
+                        "last_name": "Mercury",
+                        "username": "MrBadGuy",
+                        "type": "private"
+                    },
+                    "date": 688251600,
+                    "text": ""}
+                }
+            ]}''')
+        if edit:
+            del msg['result'][0]['message']
+            msg['result'][0]['edited_message']['text'] = case
+        elif wtf:
+            del msg['result'][0]['message']['text']
+        elif sticker:
+            del msg['result'][0]['message']['text']
+            msg['result'][0]['message']['sticker'] = {'emoji': case}
+        else:
+            msg['result'][0]['message']['text'] = case
+        return msg
+
+    def test_handle_msg_parse_edited_message(self):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(edit=True, case='test msg'), 0), 1)
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    def test_handle_msg_parse_standart_message(self):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='test msg'), 0), 1)
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    def test_handle_msg_parse_sticker_message(self):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(sticker=True, case='🏁'), 0), 1)
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    def test_handle_msg_parse_wtf_message(self):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(wtf=True, case='test msg'), 0), 1)
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.get_updates', return_value='')
+    @patch('__main__.m.send_msg', return_value='')
+    def test_handle_msg_call_bot_stop(self, get_updates, send_msg):
+        m.I.OWNER_ID = 24101991
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='/bot_stop'), 0), 0)
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.counting_start', return_value='counting_start')
+    def test_handle_msg_call_counting_start_succ(self, counting_start):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='!bot start counting for smth'), 0), 'counting_start')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.send_msg', return_value='send_msg')
+    def test_handle_msg_call_counting_start_msg_too_long(self, counting_start):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case=f'!bot start counting for {"smth"*100}'), 0), 'send_msg')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.counting_show', return_value='counting_show')
+    def test_handle_msg_call_counting_show(self, counting_show):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='!bot show smt'), 0), 'counting_show')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.counting_reset', return_value='counting_reset')
+    def test_handle_msg_call_counting_reset(self, counting_reset):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='!bot reset smth'), 0), 'counting_reset')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.counting_delete', return_value='counting_delete')
+    def test_handle_msg_call_counting_delete(self, counting_delete):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='!bot delete smth'), 0), 'counting_delete')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+    @patch('__main__.m.send_help', return_value='send_help')
+    def test_handle_msg_call_help(self, send_help):
+        self.assertEqual(m.handle_msg(handle_msg.generate_msg(case='!bot help'), 0), 'send_help')
+        self.assertEqual(m.I.LAST_ID, 1)
+
+
+class echo(unittest.TestCase):
+    def setUp(self):
+        m.msg_date = datetime.fromtimestamp(946674000).strftime('%d.%m.%Y - %H:%M:%S')
+        m.date = 946674000
+
+    def test_echo_standart(self):
+        temp = f'At {m.msg_date} user ... from ... post message #...:\n...\n{"="*70}'
+        self.assertEqual(m.echo(date=m.date), temp)
+
+    def test_echo_system(self):
+        temp = (f'\x1b[0;31;40mAt {m.msg_date} user ... from ... post message #...:\n...\n{"="*70}\x1b[0m')
+        warn = True
+        self.assertEqual(m.echo(date=m.date, warn=warn), temp)
+
+    def test_echo_all(self):
+        temp = (f'\x1b[0;31;40mAt {m.msg_date} user msg_user from chat_id post message #msg_id:\nmsg_text\n{"="*70}\x1b[0m')
+        id = 'msg_id'
+        user = 'msg_user'
+        chat = 'chat_id'
+        msg = 'msg_text'
+        warn = True
+        self.assertEqual(m.echo(id, m.date, user, chat, msg, warn), temp)
+
+
+class counting_start(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+        m.I.DB_NAME = 'test_case.db'
+        m.chat_id = 24101991
+        m.msg_date = 688251600
+
+    def test_counting_start_lazy(self):
+        m.init_db()
+        msg = '!bot start counting for lazy'
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (0, 2, 'block id 1', 3)")
+        connect.commit()
+        connect.close()
+        m.counting_start(m.chat_id, m.msg_date, msg)
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [(0, 2, 'block id 1', 3), (1, 24101991, 'lazy', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())))])
+
+    def test_counting_start_word(self):
+        m.init_db()
+        words = ('now', 'today', 'tomorrow', 'yesterday')
+        for i in words:
+            msg = f'!bot start counting since {i} for {i}'
+            m.counting_start(m.chat_id, m.msg_date, msg)
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [(0, 24101991, 'now', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple()))),
+                                  (1, 24101991, 'today', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple()))),
+                                  (2, 24101991, 'tomorrow', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())) + 86400),
+                                  (3, 24101991, 'yesterday', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())) - 86400)])
+        self.assertEqual(m.I.LAST_PRECIOUS, 4)
+
+    def test_counting_start_date(self):
+        m.init_db()
+        msg = '!bot start counting since 24.10.1991 for 24.10.1991'
+        m.counting_start(m.chat_id, m.msg_date, msg)
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [(0, 24101991, '24.10.1991', int(time.mktime(datetime.strptime("24.10.1991", "%d.%m.%Y").timetuple())))])
+        self.assertEqual(m.I.LAST_PRECIOUS, 1)
+
+    def test_counting_start_date_out(self):
+        m.init_db()
+        msg = '!bot start counting since 31.12.1950 for 01.01.1970'
+        m.counting_start(m.chat_id, m.msg_date, msg)
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result,  [(0, 24101991, '01.01.1970', 0)])
+        self.assertEqual(m.I.LAST_PRECIOUS, 1)
+
+    def test_counting_start_fail(self):
+        m.init_db()
+        msg = '!bot start counting and show'
+        m.counting_start(m.chat_id, m.msg_date, msg)
+        self.assertEqual(m.I.LAST_PRECIOUS, 0)
+
+    def tearDown(self):
+        try:
+            os.remove('test_case.db')
+        except FileNotFoundError:
+            ...
+
+
+class counting_show(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+        m.I.DB_NAME = 'test_case.db'
+        m.chat_id = 24101991
+        m.msg_date = 688251600
+
+    def test_counting_show_all(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'smth', m.msg_date))
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (1, m.chat_id, 'smth', int(time.time())))
+        connect.commit()
+        connect.close()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'all'), 1)
+
+    def test_counting_show_all_fail(self):
+        m.init_db()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'all'), 0)
+
+    def test_counting_show_smt(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'smth', m.msg_date))
+        connect.commit()
+        connect.close()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'smth'), 1)
+
+    def test_counting_show_smt_fail(self):
+        m.init_db()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'smth'), 0)
+
+    def test_show_smt_future(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'smth', int(time.time())))
+        connect.commit()
+        connect.close()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'smth'), 1)
+
+    def test_counting_show_looooong(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        for i in range(100):
+            c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (i, m.chat_id, f'{i*150}', m.msg_date))
+        connect.commit()
+        connect.close()
+        self.assertEqual(m.counting_show(m.chat_id, m.msg_date, 'all'), 1)
+
+    def tearDown(self):
+        try:
+            os.remove('test_case.db')
+        except FileNotFoundError:
+            ...
+
+
+class counting_reset(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+        m.I.DB_NAME = 'test_case.db'
+        m.chat_id = 24101991
+        m.msg_date = 688251600
+
+    def test_counting_reset_succ(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'reset', int(time.time())))
+        connect.commit()
+        m.counting_reset(m.chat_id, m.msg_date, 'reset')
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [(0, 24101991, 'reset', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())))])
+
+    def test_counting_reset_fail(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'reset', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple()))))
+        connect.commit()
+        m.counting_reset(m.chat_id, m.msg_date, 'reset fail')
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [(0, 24101991, 'reset', int(time.mktime(datetime.strptime(datetime.fromtimestamp(int(m.msg_date)).strftime("%d.%m.%Y"), "%d.%m.%Y").timetuple())))])
+
+    def tearDown(self):
+        try:
+            os.remove('test_case.db')
+        except FileNotFoundError:
+            ...
+
+
+class counting_delete(unittest.TestCase):
+    def setUp(self):
+        m.I.__init__()
+        m.I.DB_NAME = 'test_case.db'
+        m.chat_id = 24101991
+        m.msg_date = 688251600
+
+    def test_counting_delete_succ(self):
+        m.init_db()
+        connect = sqlite3.connect(m.I.DB_NAME)
+        c = connect.cursor()
+        c.execute("INSERT INTO MAIN (ID, CHAT_ID, NAME, TIME) VALUES (?, ?, ?, ?)", (0, m.chat_id, 'delete', int(time.time())))
+        connect.commit()
+        m.counting_delete(m.chat_id, m.msg_date, 'delete')
+        c.execute('SELECT * FROM MAIN')
+        result = c.fetchall()
+        connect.close()
+        self.assertEqual(result, [])
+
+    def test_counting_delete_fail(self):
+        m.init_db()
+        m.counting_delete(m.chat_id, m.msg_date, 'delete')
+
+    def tearDown(self):
+        try:
+            os.remove('test_case.db')
+        except FileNotFoundError:
+            ...
+
+if __name__ == '__main__':
+    unittest.main()
